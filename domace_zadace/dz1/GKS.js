9a3f79577b8caf3f7f5cbe5a9c23d6ac8199de2b
Jerry John Antolos <37196688+jantolos@users.noreply.github.com>
2021-10-12 12:08:08 +0200

    Licence comment

--- domace_zadace/dz1/GKS.js
+++ domace_zadace/dz1/GKS.js
@@ -1,3 +1,23 @@
+/*
+    This class is used for the purpose of converting Global coordinates 
+    (Cartesian coordinate system) into Screen coordinates.
+
+    Copyright (C) 2021. Jerry John Antolos, B.Sc.IT
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 class GKS{
     constructor(platno, x_min, x_max, y_min, y_max){
         this.platno = platno
